0d22aa5201685255ead4b9be3b04492922a40d3e
ajja99 <aja_99@hotmail.com>
2020-07-21 19:35:32 +0200

    Add forward and backpropagation

---
+++ activations.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def sigmoid(Z):
+    return 1/(1+np.exp(-Z)), Z
+
+def relu(Z):
+    return max(0, Z), Z
+
+def softmax(Z):
+    return np.exp(Z)/(np.sum(np.exp(Z))), Z
+
+def sigmoid_backward(dA, cache):
+    Z = cache
+    
+    s = sigmoid(Z)
+    dZ = dA * s * (1-s)
+
+    return dZ
+
+def relu_backward(dA, cache):
+    Z = cache
+    dZ = np.array(dA, copy=True)
+
+    dZ[Z <= 0] = 0
+
+    return dZ
--- main.py
+++ main.py
@@ -1,17 +1,120 @@
 import numpy as np
+import activations
 
 def main():
     parameters = initialize_weights([1,2,3])
-    print(parameters)
 
 def initialize_weights(layer_dims):
 
     parameters = {}
     L = len(layer_dims)
-    
+
     for l in range(1, L):
         parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1])
         parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+
+    return parameters
+
+def linear_forward(A, W, b):
+
+    Z = np.add(np.dot(W, A), b)
+    cache = (A, W, b)
+    return Z, cache
+
+def linear_activation_forward(A_prev, W, b, activation):
+
+    Z, Z_cache = linear_forward(A_prev, W, b)
+
+    if activation == 'relu':
+        A, A_cache = activations.relu(Z)
+
+    elif activation == 'sigmoid':
+        A, A_cache = activations.sigmoid(Z)
+
+    elif activation == 'softmax':
+        A_cache = activations.softmax(Z)
+
+    cache = (Z_cache, A_cache)
+
+    return A, cache
+
+def model_forward(X, parameters):
+
+    A = X
+    caches = []
+    L = len(parameters) // 2
+
+    for l in range(1, L):
+        A_prev = A
+        A, cache = linear_activation_forward(A_prev, parameters['W' + str(l)], parameters['b' + str(l)], 'relu')
+
+        caches.append(cache)
+    
+    aL, cache = linear_activation_forward(A_prev, parameters['W' + str(l)], parameters['b' + str(l)], 'sigmoid')
+    caches.append(cache)
+
+    return aL, caches
+
+def compute_cost(aL, Y):
+    m = len(Y)
+
+    cost = -(1/m) * np.sum(np.dot(Y, np.log(aL)), (1-Y) * np.log(1-aL))
+
+    return cost
+
+def linear_backward(dZ, cache):
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+
+    dW = (1/m) * np.dot(dZ, A_prev)
+    db = (1/m) * np.sum(dZ)
+    dA_prev = np.dot(W.t, dZ)
+
+    return dA_prev, W, b
+
+def linear_activation_backward(dA, cache, activation):
+    linear_cache, activation_cache = cache
+
+    if activation == 'relu':
+        dZ = np.dot(dA, activations.relu_backward(dA, activation_cache))
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    elif activation == 'sigmoid':
+        dZ = np.dot(dA, activations.sigmoid_backward(dA, activation_cache))
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    return dA_prev, dW, db
+
+def model_backward(aL, Y, caches):
+
+    grads = {}
+    L = len(caches)
+    m = aL.shape[1]
+    Y = Y.reshape(aL.shape)
+
+    daL = - (np.divide(Y, aL) - np.divide(1 - Y, 1 - aL))
+
+    current_cache = caches[L]
+    grads["dA" + str(L-1)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(daL, current_cache, 'sigmoid')
+
+    for l in reversed(range(L-1)):
+        current_cache = caches[l]
+
+        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA" + str(l+1)], current_cache, 'relu')
+
+        grads["dA" + str(l)] = dA_prev_temp
+        grads["dW" + str(l + 1)] = dW_temp
+        grads["db" + str(l + 1)] = db_temp
+
+        return grads
+
+def update_parameters(parameters, grads, learning_rate):
+
+    L = len(parameters) // 2
+
+    for l in range(L):
+        parameters["W" + str(l+1)] -= np.multiply(learning_rate, grads['dW'+ str(l)])
+        parameters["b" + str(l+1)] -= np.multiply(learning_rate, grads['db'+ str(l)])
 
     return parameters
 
